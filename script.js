236e1535521d149fa675daae6424183ec5e5c611
soph-iee <xophieadrian@gmail.com>
2022-06-25 15:17:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const hello = "Hello World";
+const myName = "Blessing Opeyemi";
+const height = "5.1ft";
+const country = "Nigeria";
+
+document.write(hello);
+document.write(` <br> My name is ${myName}`);
+document.write(` <br>I am ${height} tall`);
+document.write(` <br>I am from ${country} and that makes me a NIgerian.`);
